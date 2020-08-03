e2d0872f813911a1554db4d973e32c1468ccd57f
Ricardo Veiga <ricardo.veiga@outlook.com.br>
2020-08-03 09:11:16 -0300

    projeto backend gobarber do bootcamp gostack

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
-import 'express-async-errors';
 
+import 'express-async-errors';
+import cors from 'cors';
 import routes from './routes';
 import uploadConfig from './config/upload';
 import AppError from './errors/AppErrors';
@@ -11,6 +12,7 @@ import './database';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('./files', express.static(uploadConfig.directory));
